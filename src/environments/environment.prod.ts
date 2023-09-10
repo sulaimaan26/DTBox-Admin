c00d3e2a11c4ca2616ab598403ffe5cfbad0fb65
sulaimaan26 <sulaimaan26@gmail.com>
2023-09-10 11:24:50 +0530

    added pro env config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl:"http://vmi835174.contaboserver.net/api",
+  baseUrl:"http://vmi1453024.contaboserver.net/dtbox/api/v1",
   basehref:'/'
 };
