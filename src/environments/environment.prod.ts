50eb8abebf05d52046253613dd50a3c49b525587
sulaimaan26 <sulaimaan26@gmail.com>
2023-09-18 19:04:41 +0530

    added new prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl:"https://vmi1453024.contaboserver.net/dtbox/api/v1",
+  baseUrl:"https://api.dtpass.com/dtbox/api/v1",
   basehref:'/'
 };
